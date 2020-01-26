4b3d2ce086e85751a0f235b25acdbc3c5d420fed
Alex Pomazan <multdirectx@gmail.com>
2020-01-26 22:43:59 +0200

    added machines data

---
+++ src/components/Machines/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul v-for="machine in machines" :key="machine.id">
+      <li>{{ machine.id }}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { machines } from '@/fakeData'
+
+export default {
+  data() {
+    return {
+      machines
+    }
+  }
+}
+</script>
