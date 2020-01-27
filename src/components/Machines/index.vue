8ca655801d5d30bdd5bb94e1e296866030e57329
alexlegatics <alex.pomazan@legatics.com>
2020-01-27 12:42:27 +0200

    added cards removing ability

--- src/components/Machines/index.vue
+++ src/components/Machines/index.vue
@@ -7,7 +7,27 @@
         class="md-layout-item md-large-size-33 md-xlarge-size-33 md-medium-size-50 md-xsmall-size-100"
       >
         <md-card>
-          {{ machine.name }}
+          <md-card-header>
+            <div class="md-title">{{ machine.name }}</div>
+            <md-card-media-actions>
+              <img src="../../assets/orange-car.png" alt="" class="image" />
+            </md-card-media-actions>
+          </md-card-header>
+          <md-card-content>
+            Last known position:
+            <div>latititude: {{ machine.lastKnownPosition.latitude }}</div>
+            <div>longtitude: {{ machine.lastKnownPosition.longitude }}</div>
+          </md-card-content>
+          <md-card-actions>
+            <md-button
+              class="md-accent"
+              @click="removeMachine(machine.id)"
+              :disabled="removingDisabled"
+            >
+              Remove
+            </md-button>
+            <md-button class="md-primary">Details</md-button>
+          </md-card-actions>
         </md-card>
       </div>
     </div>
@@ -23,6 +43,16 @@ export default {
     return {
       machines
     }
+  },
+  methods: {
+    removeMachine(id) {
+      this.machines = this.machines.filter(machine => machine.id !== id)
+    }
+  },
+  computed: {
+    removingDisabled() {
+      return this.machines.length <= 1
+    }
   }
 }
 </script>
@@ -33,8 +63,18 @@ export default {
 }
 
 .md-layout-item {
-  height: 40px;
   margin-top: 8px;
   margin-bottom: 8px;
+}
+
+.md-card-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.image {
+  height: 140px;
+  width: 210px;
 }
 </style>
