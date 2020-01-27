6650c127b31aa26fa3c8172cf808eb793b310f66
Alex Pomazan <multdirectx@gmail.com>
2020-01-27 11:33:43 +0200

    Added material

--- src/components/Machines/index.vue
+++ src/components/Machines/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <ul v-for="machine in machines" :key="machine.id">
-      <li>{{ machine.id }}}</li>
-    </ul>
+  <div class="content">
+    <div
+      class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"
+    >
+      <div v-for="machine in machines" :key="machine.id" class="md-layout-item">
+        <md-card>
+          {{ machine.name }}
+        </md-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +16,7 @@
 import { machines } from '@/fakeData'
 
 export default {
+  name: 'LayoutHorizontalResponsive',
   data() {
     return {
       machines
@@ -17,3 +24,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.content {
+  padding: 30px 0;
+}
+
+.md-layout-item {
+  height: 40px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+</style>
